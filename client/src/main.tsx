ae5f7fb16f46fd6295dfdc80334c001ce6842c47
EggsDuty <eggsduty@gmail.com>
2024-03-11 00:45:24 +0200

    Added tailwind.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <h1 className="text-3xl">Silke!</h1>
   </React.StrictMode>,
 )
