6cb220fc7d1e7924e32a24cf8133cb39be24e198
IkeaPen <agne8rimk@gmail.com>
2024-03-12 02:13:34 +0200

    Created landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignUpPage from './pages/SignUpPage.tsx'
 import HomePage from './pages/HomePage.tsx'
+import LandingPage from './pages/LandingPage.tsx'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+      <Route index element={<LandingPage />} />
         <Route path="/signup" element={<SignUpPage login={false} />} />
         <Route path="/login" element={<SignUpPage login={true} />} />
         <Route path="/home" element={<HomePage />} />
