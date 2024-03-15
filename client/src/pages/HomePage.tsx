e6f1b3df12b897f3a7d8f393c7064539df26a9e7
EggsDuty <eggsduty@gmail.com>
2024-03-15 15:22:58 +0200

    Made homepage inaccessible while not logged in. Adjusted stytle of login/signup.

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import Box from "../components/HomePage/Box";
 import firebase from '../firebase.tsx';
 import Background from "../components/Background";
 //
-import { Link } from "react-router-dom"
+import { Link, Navigate } from "react-router-dom"
 
 import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
 import { useEffect, useState } from "react";
@@ -33,21 +33,25 @@ function HomePage() {
         monitorAuthState();
     }, []);
 
+    if (auth.currentUser == null) {
+        return <Navigate to="/" replace={true} />
+    }
+
     return (
         <>
-        <Background img="background_5.jpg" cover="cover" />
-        <div className="w-screen absolute text-center bg-repeat-y">
-            <Header />
-            {signedIn ? <button onClick={handleSignOut} className="text-white">Sign out for {auth.currentUser?.displayName}</button> : ""}
-            <h1 className="mt-28 text-7xl bg-gradient-to-r from-blue-400 via-gray-50 to-blue-400 w-max m-auto text-transparent bg-clip-text font-bold">Welcome{isFirstTime ? "" : " back"}!</h1>
-            <p className="mt-10 m-auto text-white font-medium">What would you like to choose?</p>
-            <div className="flex flex-wrap mt-20 mx-auto space-x-20 justify-center">
-                <Box link="/calculators" img="test_calculator_picture.png" header="Calculators" text="Choose from our wide variety of calculators???" />
-                <Box link="/whiteboard" img="test_whiteboard_picture.png" header="Whiteboard" text="Write down all your calculations while still having any calculator on the board??" />
-                <Box link="/groups" img="test_groups_picture.png" header="Groups" text="Make a group with your colleagues and work on the same board!" />
-            </div>
+            <Background img="background_5.jpg" cover="cover" />
+            <div className="w-screen absolute text-center bg-repeat-y">
+                <Header />
+                {signedIn ? <button onClick={handleSignOut} className="text-white">Sign out for {auth.currentUser?.displayName}</button> : ""}
+                <h1 className="mt-28 text-7xl bg-gradient-to-r from-blue-400 via-gray-50 to-blue-400 w-max m-auto text-transparent bg-clip-text font-bold">Welcome{isFirstTime ? "" : " back"}!</h1>
+                <p className="mt-10 m-auto text-white font-medium">What would you like to choose?</p>
+                <div className="flex flex-wrap mt-20 mx-auto space-x-20 justify-center">
+                    <Box link="/calculators" img="test_calculator_picture.png" header="Calculators" text="Choose from our wide variety of calculators???" />
+                    <Box link="/whiteboard" img="test_whiteboard_picture.png" header="Whiteboard" text="Write down all your calculations while still having any calculator on the board??" />
+                    <Box link="/groups" img="test_groups_picture.png" header="Groups" text="Make a group with your colleagues and work on the same board!" />
+                </div>
 
-        </div>
+            </div>
         </>
     )
 }
--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -7,6 +7,7 @@ import { Link, Navigate } from 'react-router-dom';
 
 import Validator from '../components/Auth/Validator.ts';
 import AuthField from '../components/Auth/AuthField.tsx';
+import Background from '../components/Background.tsx';
 
 const auth = getAuth(firebase);
 
@@ -69,22 +70,25 @@ function LoginPage() {
     }
 
     return (
-        <div className="bg-test h-screen w-screen absolute bg-cover">
-            <form onSubmit={(e) => handleLogin(e)} className="bg-indigo-800 w-1/3 m-auto mt-32 py-5 px-10 rounded-lg">
-                <Link to="/" reloadDocument={true} className="-ml-5 text-xl text-white hover:text-indigo-200">&larr; Back</Link>
-                <h1 className="text-3xl text-white mt-5">Login</h1>
-                <hr className="mb-5 mt-3" />
-                <AuthField var={emailField} validateFunction={Validator.ValidateEmail} setter={setEmailField} name="E-mail:" type="text" placeholder="example@mail.com" />
-                <AuthField var={passwordField} validateFunction={Validator.ValidatePassword} setter={setPasswordField} name="Password:" type="password" placeholder="Must contain a letter, capital letter and digit" />
-                <button type="submit" className="py-2 px-6 mt-5 rounded-lg text-white bg-indigo-500 border border-indigo-300 hover:border-white hover:bg-indigo-200 hover:text-black">Login</button>
-                {loginError !== "" ?
-                    <div className="bg-indigo-900 my-3 p-3 pl-1 mt-10 rounded-sm text-left border-2 border-red-600">
-                        <p className="text-white text-2xl">{errorMap[loginError]}</p>
-                    </div> :
-                    ""
-                }
-            </form>
-        </div>
+        <>
+            {/*<Background img="background_5.jpg" cover="cover" />*/}
+            <div className="bg-test bg-cover h-screen w-screen absolute">
+                <form onSubmit={(e) => handleLogin(e)} className="bg-indigo-900 w-1/3 m-auto mt-32 pt-5 pb-10 px-10 rounded-lg">
+                    <Link to="/" className="-ml-5 text-xl text-white hover:text-indigo-200">&larr; Back</Link>
+                    <h1 className="text-3xl text-white mt-5">Login</h1>
+                    <hr className="mb-5 mt-3" />
+                    <AuthField var={emailField} validateFunction={Validator.ValidateEmail} setter={setEmailField} name="E-mail:" type="text" placeholder="example@mail.com" />
+                    <AuthField var={passwordField} validateFunction={Validator.ValidatePassword} setter={setPasswordField} name="Password:" type="password" placeholder="Must contain a letter, capital letter and digit" />
+                    <button type="submit" className="py-2 px-6 mt-5 rounded-lg text-white bg-indigo-500 border border-indigo-300 hover:border-white hover:bg-indigo-200 hover:text-black">Login</button>
+                    {loginError !== "" ?
+                        <div className="bg-indigo-900 my-3 p-3 pl-1 mt-10 rounded-sm text-left border-2 border-red-600">
+                            <p className="text-white text-2xl">{errorMap[loginError]}</p>
+                        </div> :
+                        ""
+                    }
+                </form>
+            </div>
+        </>
     );
 }
 
