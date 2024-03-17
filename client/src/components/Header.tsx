41c13c6cfdf1b7c95f8ea0215b1b4c11a520d527
EggsDuty <eggsduty@gmail.com>
2024-03-17 22:16:26 +0200

    Fixed header profile overlay thing-y not working on the whiteboard.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -16,7 +16,7 @@ function Header(props: Props) {
 
     return (
         <>
-            <div className={`flex flex-row min-h-10 ${props.transparent || props.transparent == null ? "bg-black bg-opacity-70 backdrop-blur-sm" : "bg-[#100524]"} sticky top-0`}>
+            <div className={`flex flex-row min-h-10 ${props.transparent || props.transparent == null ? "bg-black bg-opacity-70 backdrop-blur-sm" : "bg-[#100524]"} sticky top-0 z-50`}>
                 {//<h1 className="text-white mx-6 text-3xl">Silkė</h1>
                 }
                 <img className="mx-6 h-9 w-auto" src="/fish1.png" />
@@ -26,7 +26,7 @@ function Header(props: Props) {
                 <Link to='/groups' className="text-white leading-9 px-10 border-r-2 transition-all duration-1000 hover:bg-gradient-to-t hover:from-gray-400 hover:to-50% hover:to-transparent">Groups</Link>
                 <div className="flex flex-row ml-auto mr-4 group w-[12%]">
                     <img src="placeholder.jpg" className="rounded-full h-8 w-auto mt-[2px] ml-auto mr-4" />
-                    <div className="mt-10 bg-[#100524] rounded-sm w-[12%] fixed flex flex-col invisible hover:visible group-hover:visible p-2 text-white text-left">
+                    <div className="mt-10 bg-[#100524] rounded-sm w-[12%] fixed flex flex-col invisible hover:visible group-hover:visible p-2 text-white text-left bg-opacity-90">
                         <p className="text-lg text-white">{auth.currentUser?.displayName}</p>
                         <p className="text-sm mb-3 text-neutral-300">{auth.currentUser?.email}</p>
                         <Link to='/profile' className="mb-5 text-lg text-center text-indigo-200 hover:text-indigo-100">Edit Profile</Link>
--- client/src/pages/WhiteBoard.tsx
+++ client/src/pages/WhiteBoard.tsx
@@ -2,12 +2,14 @@ import { Tldraw } from 'tldraw'
 import 'tldraw/tldraw.css'
 import Header from "../components/Header"
 
-function Whiteboard () {
-    return (
-		<div style={{ position: 'fixed', inset: 0 }}>
+function Whiteboard() {
+	return (
+		<>
 			<Header transparent={false} />
-			<Tldraw />
-		</div>
+			<div style={{ position: 'fixed', inset: 0 }}>
+				<Tldraw />
+			</div>
+		</>
 	)
 }
 
