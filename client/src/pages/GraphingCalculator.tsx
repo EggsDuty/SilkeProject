125979908497a136f83d9f1bae1cf6f11fa49418
CowRules <roskeats@gmail.com>
2024-03-20 05:45:43 +0200

    Added checkbox to make drawn function closed. Added polar, parametric, vector and text function types

--- client/src/pages/GraphingCalculator.tsx
+++ client/src/pages/GraphingCalculator.tsx
@@ -7,9 +7,13 @@ import Select from "react-dropdown-select";
 function GraphingCalculators() {
   //Equation components
   interface Equation {
-    expression: string;
-    type: "linear" | "implicit";
+    Input1: string;
+    type: "linear" | "implicit" | "polar" | "parametric" | "vector" | "text";
     color?: string;
+    closed?: boolean;
+    Input2?: string;
+    Input3?: string;
+    Input4?: string;
   }
   //Function type option list
   const optionsType = [
@@ -21,10 +25,25 @@ function GraphingCalculators() {
       id: 2,
       name: "implicit",
     },
+    {
+      id: 3,
+      name: "polar",
+    },
+    {
+      id: 4,
+      name: "parametric",
+    },
+    {
+      id: 5,
+      name: "vector",
+    },
+    {
+      id: 6,
+      name: "text",
+    },
   ];
   //Function color option list
   const optionsColor = [
-    
     {
       id: 1,
       name: "green",
@@ -56,14 +75,14 @@ function GraphingCalculators() {
   ];
   //Initial equations
   const [equations, setEquations] = useState<Equation[]>([
-    { expression: "log(3*x) / log(6)", type: "linear" },
-    { expression: "2*x", type: "linear" },
-    { expression: "x^2", type: "linear" },
-    { expression: "2^x", type: "linear" },
-    { expression: "exp(x)", type: "linear" },
-    { expression: "sqrt(x)", type: "linear" },
-    { expression: "nthRoot(x,3)", type: "linear" },
-    { expression: "x*x+y*y-1", type: "implicit" },
+    { Input1: "log(3*x) / log(6)", type: "linear" },
+    { Input1: "2*x", type: "linear" },
+    { Input1: "x^2", type: "linear" },
+    { Input1: "2^x", type: "linear" },
+    { Input1: "exp(x)", type: "linear" },
+    { Input1: "sqrt(x)", type: "linear" },
+    { Input1: "nthRoot(x,3)", type: "linear" },
+    { Input1: "x*x+y*y-1", type: "implicit" },
   ]);
   //Graph
   const rootEl = useRef<HTMLDivElement | null>(null);
@@ -75,30 +94,86 @@ function GraphingCalculators() {
           target: rootEl.current,
           width: rootEl.current.clientWidth,
           height: rootEl.current.clientHeight,
-          yAxis: { domain: [-1, 9] },
+
           grid: true,
           //Going through all inputs
-          data: equations.map(({ expression, type, color }) => {
-            //Checking type of function (linear needs to have GraphType: 'polyline')
-            if (type === "linear") {
+          data: equations.map(
+            ({
+              Input1: Input1,
+              type,
+              color,
+              closed,
+              Input2: Input2,
+              Input3,
+              Input4,
+            }) => {
+              //Checking type of function (linear needs to have GraphType: 'polyline')
+              if (type === "linear") {
+                return {
+                  fnType: type,
+                  fn: Input1,
+                  graphType: "polyline",
+                  color: color,
+                  closed: closed,
+                };
+              } else if (type === "implicit") {
+                return {
+                  fnType: type,
+                  fn: Input1,
+                  color: color,
+                  closed: closed,
+                };
+              } else if (type === "polar") {
+                return {
+                  fnType: type,
+                  r: Input1,
+                  graphType: "polyline",
+                  color: color,
+                  closed: closed,
+                };
+              } else if (type === "text") {
+                return {
+                  graphType: "text",
+                  text: Input1,
+                  location: [
+                    Input2 ? parseFloat(Input2) : 0, // Provide a default value of 0 if Input2 is undefined
+                    Input3 ? parseFloat(Input3) : 0, // Provide a default value of 0 if Input3 is undefined
+                  ],
+                  color: color,
+                  closed: closed,
+                };
+              } else if (type === "vector") {
+                return {
+                  fnType: "vector",
+                  graphType: "polyline",
+
+                  vector: [
+                    Input1 ? parseFloat(Input1) : 0, // Provide a default value of 0 if Input2 is undefined
+                    Input2 ? parseFloat(Input2) : 0, // Provide a default value of 0 if Input3 is undefined
+                  ],
+                  offset: [
+                    Input3 ? parseFloat(Input3) : 0, // Provide a default value of 0 if Input2 is undefined
+                    Input4 ? parseFloat(Input4) : 0, // Provide a default value of 0 if Input3 is undefined
+                  ],
+                  color: color,
+                };
+              }
               return {
                 fnType: type,
-                fn: expression,
+                x: Input1,
+                y: Input2,
                 graphType: "polyline",
                 color: color,
-              };
-            } else {
-              return {
-                fnType: type,
-                fn: expression,
-                color: color,
+                closed: closed,
               };
             }
-          }),
+          ),
           tip: {
             renderer: (x: number, y: number, index: number) => {
               const equation = equations[index]; // Get the current equation
-              return `f(x)=: ${equation.expression}, x: ${x.toFixed(2)}, y: ${y.toFixed(2)}`; //Setting format of tip
+              return `f(x)=: ${equation.Input1}, x: ${x.toFixed(
+                2
+              )}, y: ${y.toFixed(2)}`; //Setting format of tip
             },
           },
         });
@@ -106,15 +181,42 @@ function GraphingCalculators() {
     } catch (e) {}
   }, [equations]);
   //Handling equation input change
-  const handleInputChange =
+  const handleInputChange1 =
     (index: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
       const newEquations = [...equations];
-      newEquations[index].expression = e.target.value;
+      newEquations[index].Input1 = e.target.value;
+      setEquations(newEquations);
+    };
+  const handleInputChange2 =
+    (index: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newEquations = [...equations];
+      newEquations[index].Input2 = e.target.value;
+
+      setEquations(newEquations);
+    };
+  const handleInputChange3 =
+    (index: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newEquations = [...equations];
+      newEquations[index].Input3 = e.target.value;
+
+      setEquations(newEquations);
+    };
+  const handleInputChange4 =
+    (index: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newEquations = [...equations];
+      newEquations[index].Input4 = e.target.value;
+
+      setEquations(newEquations);
+    };
+  const handleClosedInputChange =
+    (index: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newEquations = [...equations];
+      newEquations[index].closed = e.target.checked;
       setEquations(newEquations);
     };
   //Adding new input and adding default data
   const addEquation = () => {
-    setEquations([...equations, { expression: "", type: "linear" }]);
+    setEquations([...equations, { Input1: "", type: "linear" }]);
   };
   //Handling type input change
   const handleTypeInputChange = (index: number) => (selectedOptions: any) => {
@@ -123,7 +225,11 @@ function GraphingCalculators() {
     if (selectedOptions && selectedOptions.length > 0) {
       newEquations[index].type = selectedOptions[0].name as
         | "linear"
-        | "implicit";
+        | "implicit"
+        | "polar"
+        | "parametric"
+        | "text"
+        | "vector";
     } else {
       // If no option is selected, clear the color
       newEquations[index].color = undefined;
@@ -177,7 +283,8 @@ function GraphingCalculators() {
                   input your functions, it consists of:<br></br>
                   1. User's guide, where you can learn how to use graphic
                   calculator<br></br>
-                  2. Equation input with 2 selection boxes(function type and color) and "Remove" button<br></br>
+                  2. Equation input with 2 selection boxes(function type and
+                  color) and "Remove" button<br></br>
                   3. "Add Equation" button<br></br>
                   On the right side of the screen are the results of your input.
                 </p>
@@ -188,10 +295,11 @@ function GraphingCalculators() {
                   When you load the page, you can see 8 equation inputs
                   showcasing examples of different functions.<br></br>
                   To see your function, enter it in the textbox.<br></br>
-                  Choose your function type(default: linear) and color from
-                  the selection boxes<br></br>
+                  Choose your function type(default: linear) and color from the
+                  selection boxes<br></br>
                   If you drag your mouse on the drawn function, you will see
-                  your input and [x, y] coordinates(does not work for implicit functions).<br></br>
+                  your input and [x, y] coordinates(does not work for implicit
+                  functions).<br></br>
                   To navigate through the graph, hold your left mouse button and
                   drag your mouse.<br></br>
                   To zoom in you can either double tap your left mouse button or
@@ -205,8 +313,9 @@ function GraphingCalculators() {
                 </p>
                 <h1 className="text-lg m-1 md:font-semibold italic">
                   NOTE: Do not leave empty input boxes and be sure to enter your
-                  equation correctly(only x variables are accepted for linear functions), otherwise
-                  the graph will be unresponsive until you correct it
+                  equation correctly(only x variables are accepted for linear
+                  functions), otherwise the graph will be unresponsive until you
+                  correct it
                 </h1>
                 <h1 className="text-3xl text-center m-3 md:font-bold">
                   OPERATORS
@@ -244,59 +353,160 @@ function GraphingCalculators() {
           >
             {}
           </Popup>
-          {equations.map(({ expression, type, color }, index) => (
-            <div className="flex my-4" key={index}>
-              <label className="text-white" htmlFor={`equationInput-${index}`}/*Input label */> 
-                Equation {index + 1}:{" "}
-              </label>
-              <input //Equation input
-                type="text"
-                id={`equationInput-${index}`}
-                value={expression}
-                onChange={handleInputChange(index)}
-                className="border border-black bg-secondaryColor text-red-100 h-5/6"
-              />
-              
-              <Select //Type select
-                className="text-red-200 bg-extraColor1"
-                options={optionsType}
-                color="green"
-                searchable={false}
-                clearable={true}
-                closeOnClickInput={true}
-                closeOnSelect={true}
-                labelField="name"
-                valueField="id"
-                onChange={(selectedOptions) =>
-                  handleTypeInputChange(index)(selectedOptions)
-                }
-                values={[{id:1, name: type}]}
-              />
-              ;
-              <Select //Color select
-                className="text-red-200 bg-extraColor1"
-                options={optionsColor}
-                color="green"
-                searchable={false}
-                clearable={true}
-                closeOnClickInput={true}
-                closeOnSelect={true}
-                labelField="name"
-                valueField="id"
-                onChange={(selectedOptions) =>
-                  handleColorInputChange(index)(selectedOptions)
-                }
-                values={[]}
-              />
-              ;
-              <button //Button to remove input
-                className="border-2 border-indigo-950 my-2 bg-primaryColor text-white"
-                onClick={() => removeEquation(index)}
-              >
-                Remove
-              </button>
-            </div>
-          ))}
+          {equations.map(
+            (
+              {  Input1, type, Input2, Input3, Input4 },
+              index
+            ) => (
+              <div className=" my-4" key={index}>
+                <label
+                  className="text-white"
+                  htmlFor={`equationInput-${index}`} /*Input label */
+                >
+                  Input {index + 1}:{" "}
+                </label>
+                {type === "parametric" ? (
+                  <>
+                    <input
+                      type="text"
+                      id={`parametricX-${index}`}
+                      value={Input1}
+                      onChange={handleInputChange1(index)}
+                      className="border border-black bg-secondaryColor text-red-100 h-5/6"
+                      placeholder="X input"
+                    />
+                    <input
+                      type="text"
+                      id={`parametricY-${index}`}
+                      value={Input2}
+                      onChange={handleInputChange2(index)}
+                      className="border border-black bg-secondaryColor text-red-100 h-5/6"
+                      placeholder="Y input"
+                    />
+                  </>
+                ) : type === "text" ? (
+                  <>
+                    <input
+                      type="text"
+                      id={`Text-${index}`}
+                      value={Input1}
+                      onChange={handleInputChange1(index)}
+                      className="border border-black bg-secondaryColor text-red-100 h-5/6"
+                      placeholder="text"
+                    />
+                    <input
+                      type="text"
+                      id={`CoordX-${index}`}
+                      value={Input2}
+                      onChange={handleInputChange2(index)}
+                      className="border border-black bg-secondaryColor text-red-100 h-5/6 w-1/12"
+                      placeholder="X"
+                    />
+                    <input
+                      type="text"
+                      id={`CoordY-${index}`}
+                      value={Input3}
+                      onChange={handleInputChange3(index)}
+                      className="border border-black bg-secondaryColor text-red-100 h-5/6 w-1/12"
+                      placeholder="Y "
+                    />
+                  </>
+                ) : type === "vector" ? (
+                  <>
+                    <input
+                      type="text"
+                      id={`LengthX-${index}`}
+                      value={Input1}
+                      onChange={handleInputChange1(index)}
+                      className="border border-black bg-secondaryColor text-red-100 h-5/6 w-2/12"
+                      placeholder="size-X"
+                    />
+                    <input
+                      type="text"
+                      id={`LengthY-${index}`}
+                      value={Input2}
+                      onChange={handleInputChange2(index)}
+                      className="border border-black bg-secondaryColor text-red-100 h-5/6 w-2/12"
+                      placeholder="sizeY"
+                    />
+                    <input
+                      type="text"
+                      id={`OffsetX-${index}`}
+                      value={Input3}
+                      onChange={handleInputChange3(index)}
+                      className="border border-black bg-secondaryColor text-red-100 h-5/6 w-2/12"
+                      placeholder="offset-X "
+                    />
+                    <input
+                      type="text"
+                      id={`OffsetY-${index}`}
+                      value={Input4}
+                      onChange={handleInputChange4(index)}
+                      className="border border-black bg-secondaryColor text-red-100 h-5/6 w-2/12"
+                      placeholder="offset-Y"
+                    />
+                  </>
+                ) : (
+                  <input
+                    type="text"
+                    id={`equationInput-${index}`}
+                    value={Input1}
+                    onChange={handleInputChange1(index)}
+                    placeholder="Input"
+                    className="border border-black bg-secondaryColor text-red-100 h-5/6"
+                  />
+                )}
+                <button //Button to remove input
+                  className="border-2 border-indigo-950 my-2 bg-primaryColor text-white"
+                  onClick={() => removeEquation(index)}
+                >
+                  Remove
+                </button>
+                <div className="flex">
+                  <p className="text-white my-auto mx-2">Function type:</p>
+                  <Select //Type select
+                    className="text-red-200 bg-extraColor1"
+                    options={optionsType}
+                    color="green"
+                    searchable={false}
+                    clearable={true}
+                    closeOnClickInput={true}
+                    closeOnSelect={true}
+                    labelField="name"
+                    valueField="id"
+                    onChange={(selectedOptions) =>
+                      handleTypeInputChange(index)(selectedOptions)
+                    }
+                    values={[{ id: 1, name: type }]}
+                  />
+                  ;<p className="text-white my-auto mx-2"> Color: </p>
+                  <Select //Color select
+                    className="text-red-200 bg-extraColor1"
+                    options={optionsColor}
+                    color="green"
+                    searchable={false}
+                    clearable={true}
+                    closeOnClickInput={true}
+                    closeOnSelect={true}
+                    labelField="name"
+                    valueField="id"
+                    onChange={(selectedOptions) =>
+                      handleColorInputChange(index)(selectedOptions)
+                    }
+                    values={[]}
+                  />
+                  ;<p className="my-auto text-white mx-2">Closed: </p>
+                  <input //Equation input
+                    type="checkbox"
+                    id={`equationInput-${index}`}
+                    checked={equations[index].closed || false}
+                    onChange={handleClosedInputChange(index)}
+                    className="border border-black bg-secondaryColor text-red-100 h-5/6 my-auto"
+                  />
+                </div>
+              </div>
+            )
+          )}
 
           <button //Button to add input
             className="border-2 border-black my-2 bg-primaryColor text-white w-1/3"
