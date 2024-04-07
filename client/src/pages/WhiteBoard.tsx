30b3d640745803e489cda1a6f1094afa45f19f00
Dainora Kairaityte <dainora.kairaityte@gmail.com>
2024-04-07 19:55:16 +0300

    Installed Yjs library and tried implementing it for whiteboard

--- client/src/pages/WhiteBoard.tsx
+++ client/src/pages/WhiteBoard.tsx
@@ -1,18 +1,62 @@
-import { Tldraw } from 'tldraw'
+import { Tldraw, track, useEditor } from 'tldraw'
 import 'tldraw/tldraw.css'
+import { useYjsStore } from '../useYjsStore.ts'
 import Header from "../components/Header"
-import Sidebar from '../components/Whiteboard/Sidebar'
+import Sidebar  from '../components/Whiteboard/Sidebar.tsx'
 
-function Whiteboard() {
+const HOST_URL =
+	import.meta.env.MODE === 'development'
+		? 'ws://localhost:1234'
+		: 'wss://demos.yjs.dev'
+
+export default function YjsExample() {
+	const store = useYjsStore({
+		roomId: 'example17',
+		hostUrl: HOST_URL,
+	})
+
 	return (
 		<>
 			<Header transparent={false} />
-			<Sidebar />
-			<div className="ml-32" style={{ position: 'fixed', inset: 0 }}>
-				<Tldraw />
-			</div>
+            <Sidebar />
+            <div className="ml-32" style={{ position: 'fixed', inset: 0 }}>
+
+			<Tldraw
+				autoFocus
+				//store={store}
+				components={{
+					SharePanel: NameEditor,
+				}}
+			/>
+		</div>
 		</>
 	)
 }
 
-export default Whiteboard;
+const NameEditor = track(() => {
+	const editor = useEditor()
+
+	const { color, name } = editor.user.getUserPreferences()
+
+	return (
+		<div style={{ pointerEvents: 'all', display: 'flex' }}>
+			<input
+				type="color"
+				value={color}
+				onChange={(e) => {
+					editor.user.updateUserPreferences({
+						color: e.currentTarget.value,
+					})
+				}}
+			/>
+			<input
+				value={name}
+				onChange={(e) => {
+					editor.user.updateUserPreferences({
+						name: e.currentTarget.value,
+					})
+				}}
+			/>
+		</div>
+	)
+})
