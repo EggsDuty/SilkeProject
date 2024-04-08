b7c6ab465358f07582dde05ae141b63e2f0e9821
IkeaPen <agne8rimk@gmail.com>
2024-04-08 18:58:31 +0300

    Fixed group box y align

--- client/src/components/Groups/GroupBox.tsx
+++ client/src/components/Groups/GroupBox.tsx
@@ -13,8 +13,8 @@ function GroupsBox(props: Props) {
 
     return (
         <div className="flex flex-row bg-blue-400 bg-opacity-20 pb-3 px-3 h-14 items-center text-white border-b-[1px]">
-            <div className="w-[20%]">
-                <Link to={`/group/${props.groupID}`} className="truncate text-left mt-2 pl-3 hover:underline"> {props.groupName}</Link>
+            <div className="w-[20%] mt-2 pl-3">
+                <Link to={`/group/${props.groupID}`} className="truncate text-left hover:underline"> {props.groupName}</Link>
             </div>
 
             <p className={`${"s" + props.index.toString()} truncate text-left min-w-[60%] max-w-[60%] mt-2 text-gray-400 peer`}>{props.description}</p>
