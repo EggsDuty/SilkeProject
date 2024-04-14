7b7b14ef50bb0e282bb30d51e4edaf040fbfc6b2
IkeaPen <agne8rimk@gmail.com>
2024-04-15 02:43:48 +0300

    Fixed glitch with profile picture bugging out in members list

--- client/src/components/Groups/MemberBox.tsx
+++ client/src/components/Groups/MemberBox.tsx
@@ -24,12 +24,12 @@ function MemberBox(props: Props){
 
     return (
         <div className="flex flex-row w-full bg-blue-400 bg-opacity-20 pb-3 px-3 h-14 items-center text-white border-b-[1px]">
-            <img src={props.image} className="rounded-full h-8 w-8 mt-[11px] mr-4" />
+            <img src={props.image} className="rounded-full h-8 min-w-8 mt-[11px] mr-4" />
             <div className="min-w-[70%] mt-2 pl-3 text-left">
                 <Link to={`/profile/${props.memberID}`} className="truncate hover:underline">{props.memberName}</Link>
             </div>
             {props.isLeader ?
-                <p className="truncate text-right text-gray-400 min-w-[20%] max-w-[20%] mt-2 pr-3">Leader</p>
+                <p className="truncate text-right text-gray-400 w-[20%] mt-2 pr-3">Leader</p>
                 :
                 ""
             }
